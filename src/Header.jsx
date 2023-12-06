eb0a408c6343d4eabfef9435e2ec56bbbf042cc6
Raees <mraiskundi@gmail.com>
2023-12-06 15:50:33 +0500

    Home Work

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Header = () => {
+  return (
+    <div className='container d-flex w-100 '>
+      <header className='container bg-info align-content-center '>
+        <h2>Track Your Expenses</h2>
+      </header>
+    </div>
+  )
+}
+
+export default Header
