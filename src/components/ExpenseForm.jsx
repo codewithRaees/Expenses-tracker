265d72433f4faf3c3bdf6742204701992775d28f
codewithRaees <mraiskundi@gmail.com>
2023-12-13 11:26:29 +0500

    Office work December132023

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -2,43 +2,32 @@ import React, { useState } from 'react'
 
 const ExpenseForm = ({setexpenses}) => {
 
-    const [formData, setFormData] = useState({
-      title: '',
-      category: '',
-      amount: '',
-      
-    });
-  
-  
-    // Function to handle form input changes
-    const handleInputChange = (event) => {
-      const { name, value } = event.target;
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    };
-  
-  const handlesubmision = (e)=>{
+  const handleform = (e) => {
     e.preventDefault()
-    console.log('Form Data:', formData);
-    setexpenses((prestate)=>[...prestate , formData])
-    
-  
-}
+   const expense = {...getFormData(e.target), id:crypto.randomUUID()}
+    setexpenses((prestate) => [...prestate ,expense])
+    e.target.reset()
+  }
+  const getFormData = (form) => {
+    const formData = new FormData(form)
+    const data = {}
+      for (const [key , value] of formData.entries()) {
+      
+      data[key] = value
+    }
+    return data
+  }
   return (
     <>
       <div className="expense-form d-flex flex-column bg-primary">
-        <form className="expense-form" onSubmit={ handlesubmision}>
+        <form className="expense-form" onSubmit={handleform}>
           <div className="input-container">
             <label htmlFor="title">Title</label>
-            <input id="title" name="title" type='text'
-          value={formData.title}  onChange={handleInputChange}/>
+            <input id="title" name="title" type='text'/>
           </div>
           <div className="input-container">
             <label htmlFor="category">Category</label>
-            <select id='category' name="category"
-          value={formData.category} onChange={handleInputChange}>
+            <select id='category' name="category">
               <option value="" hidden>Select Category</option>
               <option value="grocery">Grocery</option>
               <option value="clothes">Clothes</option>
@@ -49,8 +38,7 @@ const ExpenseForm = ({setexpenses}) => {
           </div>
           <div className="input-container">
             <label htmlFor="amount">Amount</label>
-            <input id="amount" name="amount"
-          value={formData.amount} onChange={handleInputChange}/>
+            <input id="amount" name="amount"/>
           </div>
           <button  className="add-btn btn btn-warning ">Add</button>
         </form>
