7a36689811b8fda238c55491bee7fa2dcf91bb72
codewithRaees <mraiskundi@gmail.com>
2023-12-16 13:12:14 +0500

    OfficeWork16Dec2023

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,25 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-const ExpenseForm = ({setexpenses}) => {
+const ExpenseForm = ({ setexpenses }) => {
   // const [title , setTitle] = useState('')
   // const [category , setCategory] = useState('')
   // const [amount , setAmount] = useState('')
-  const [expense , setExpense] = useState({
-    title:'',
+  const [expense, setExpense] = useState({
+    title: '',
     category: '',
     amount: ''
   })
-  const handleform = (e) => {
-    
-    e.preventDefault()
+  const[errors , setErrors] = useState({})
+  const formValidation = (formdata) => {
+    const errorsData = {}
+    if(!expense.title)
+    {
+      errorsData.title = "Title is Required"
+    }
+    if(!expense.category)
+    {
+      errorsData.category = "Please select a Categoery"
+    }
+    if(!expense.amount)
+    {
+      errorsData.amount = "Title is Amount"
+    }
+    setErrors(errorsData)
+    return errorsData
    
-    setexpenses((prestate) => [...prestate ,{...expense,id:crypto.randomUUID() }])
-   setExpense({
-    title:'',
-    category: '',
-    amount: ''
-   })
   }
+  const handleform = (e) => {
+    e.preventDefault()
+    const validateResult = formValidation(expense)
+    if(Object.keys(validateResult).length) return
+    setexpenses((prestate) => [...prestate, { ...expense, id: crypto.randomUUID() }])
+    setExpense({
+      title: '',
+      category: '',
+      amount: ''
+    })
+  }
+  const handleChange = (e) => {
+    setErrors({})
+    const { name,value} = e.target;
+    setExpense((prevstate) => ({ ...prevstate, [name]: value }))
+  }
+  // const titleRef = useRef('hi')
+  // console.log(titleRef.current)
+  // useEffect(()=>{
+  //   titleRef.current.style.backgroundColor = 'pink'
+  // })
 
   return (
     <>
@@ -27,15 +56,18 @@ const ExpenseForm = ({setexpenses}) => {
         <form className="expense-form" onSubmit={handleform}>
           <div className="input-container">
             <label htmlFor="title">Title</label>
-            <input id="title" 
-             name='title' 
-             value={expense.title} 
-             onChange={(e)=> setExpense((prevstate)=> ({...prevstate, title:e.target.value}))}/>
+            <input id="title"
+              name='title'
+              value={expense.title}
+              onChange={handleChange}
+            //  ref={titleRef}
+            />
+            <p className='error text-warning fs-6'>{errors.title}</p>
           </div>
           <div className="input-container">
             <label htmlFor="category">Category</label>
-            <select id='category' name="category" value={expense.category} 
-             onChange={(e)=> setExpense((prevstate)=> ({...prevstate, category:e.target.value}))}>
+            <select id='category' name="category" value={expense.category}
+              onChange={handleChange}>
               <option value="" hidden>Select Category</option>
               <option value="grocery">Grocery</option>
               <option value="clothes">Clothes</option>
@@ -43,13 +75,15 @@ const ExpenseForm = ({setexpenses}) => {
               <option value="education">Education</option>
               <option value="medicine">Medicine</option>
             </select>
+            <p className='error text-warning fs-6'>{errors.category}</p>
           </div>
           <div className="input-container">
             <label htmlFor="amount">Amount</label>
-            <input id="amount" name="amount" value={expense.amount} 
-             onChange={(e)=> setExpense((prevstate)=> ({...prevstate, amount:e.target.value}))}/>
+            <input id="amount" name="amount" value={expense.amount}
+              onChange={handleChange} />
+              <p className='error text-warning fs-6'>{errors.amount}</p>
           </div>
-          <button  className="add-btn btn btn-warning ">Add</button>
+          <button className="add-btn btn btn-warning ">Add</button>
         </form>
 
       </div>
